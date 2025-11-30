95b4b6e6dcf64f31828eb64308dc2b5647fd95d6
neamatullah01 <neamat22205101908@diu.edu.bd>
2025-11-30 18:39:52 +0600

    add todos post and get method

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { Result } from "./../node_modules/@types/pg/index.d";
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
@@ -48,6 +49,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+//*users table crud operation:
+
+//post users:
 app.post("/users", async (req: Request, res: Response) => {
   const { name, email } = req.body;
   try {
@@ -70,6 +74,7 @@ app.post("/users", async (req: Request, res: Response) => {
   }
 });
 
+//get all users:
 app.get("/users", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`
@@ -88,6 +93,7 @@ app.get("/users", async (req: Request, res: Response) => {
   }
 });
 
+///get single user
 app.get("/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
@@ -115,6 +121,7 @@ app.get("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+//update single user data:
 app.put("/users/:id", async (req: Request, res: Response) => {
   const { name, email } = req.body;
   try {
@@ -143,6 +150,7 @@ app.put("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+//delete single user:
 app.delete("/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
@@ -160,6 +168,50 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
     res.status(200).json({
       success: true,
       message: "user Deleted",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//*todos table crud operation:
+
+//post todos data:
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `
+      INSERT INTO todos(user_id, title) VALUES($1,$2) RETURNING *
+      `,
+      [user_id, title]
+    );
+    res.status(201).json({
+      success: true,
+      message: "Todos created",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//get todos data:
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+      SELECT * FROM todos
+      `);
+    res.status(200).json({
+      success: true,
+      message: "Data retrieved",
       data: result.rows,
     });
   } catch (error: any) {
